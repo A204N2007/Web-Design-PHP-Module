d9b48e42dfe432f48d6679d17a2d5a60c28e49b4
Ahmed Azdan Adam <113350077+A204N2007@users.noreply.github.com>
2022-09-14 18:53:46 -0700

    Week 3 Class Activities
    Week 3 Class Activities

---
+++ Week 3/else.php
@@ -0,0 +1,11 @@
+<?php
+$roll=rand(1, 6);
+
+echo "You rolled a" .$roll . "\n";
+
+if ($roll == 6) {
+    echo 'You win!';
+}else{
+    echo "You Lose!";
+}
+?>
---
+++ Week 3/switch.php
@@ -0,0 +1,30 @@
+<?php
+$day=5;
+switch ($day) {
+    case 0:
+        echo "Sunday";
+    break;
+    case 1:
+        echo "Monday";
+        break;
+    case 2:
+        echo "Tuesday";
+        break;
+    case 3:
+        echo "Wednesday";
+        break;
+    case 4:
+        echo "Thursday";
+        break;
+    case 5:
+        echo "Friday";
+        break;
+    case 6:
+        echo "Saturday";
+        break;
+    default:
+        echo "Not a week day!";
+            
+    }
+
+?>
