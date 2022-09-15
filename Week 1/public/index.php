e3209707ad407d8595c098cc58d70cd70c2905c5
Ahmed Azdan Adam <113350077+A204N2007@users.noreply.github.com>
2022-09-14 18:58:18 -0700

    Week 1 PHP
    Week 1 PHP

---
+++ Week 1/public/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Profile</title>
+</head>
+
+<body>
+
+<?php
+ echo '<h1 style="color:blue"><marquee>About Myself</marquee></h1>';
+ echo '<p>Name: Ahmed Azdan Adam</p>';
+ echo '<p>Age: 14 and 1 month</p>';
+ echo '<p>Description</p>';
+ echo '<p>My Lucky Number' . rand(10, 20) .' </p>';
+
+?>
+</body>
+</html>
+
